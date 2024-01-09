f01ecf157109a8e781b4559f1a738abdd2087e2b
hoan <103342095+Hoan-K-Le@users.noreply.github.com>
2024-01-09 01:23:03 -0800

    added framer motion

--- app/components/HeroComponent/index.tsx
+++ app/components/HeroComponent/index.tsx
@@ -5,18 +5,18 @@ import Link from "next/link";
 
 export const HeroComponent = () => {
   return (
-    <div className="py-20 flex justify-center border-b border-gray-700">
+    <div className="py-12 flex justify-center">
       <div className="flex justify-center w-2/3 items-center gap-2">
         <div className="flex flex-col w-2/3 items-center">
-          <h1 className="text-6xl p-4 font-bold bg-gradient-to-r from-white via-gray-200 to-gray-400 bg-clip-text text-transparent tracking-wide">
+          <h1 className="text-6xl p-4 font-bold bg-gradient-to-r from-white via-gray-400 to-gray-500 bg-clip-text text-transparent tracking-wide">
             Learn,Grow,Excel!
           </h1>
-          <p className="text-center text-xl  text-gray-300 tracking-wide">
+          <p className="text-center text-xl  text-gray-200 tracking-wide leading-7">
             Effortlessly start your journey into front-end web development with
             all the essential tools and guidance in one place. No more endless
             searching for information - everything you need is right here!
           </p>
-          <div className="mt-4 flex flex-col justify-center items-center gap-4">
+          <div className="mt-4 flex flex-col justify-center items-center gap-6">
             <p className="font-semibold text-gray-300 text-sm">
               Open source project, more than welcome to contribute!
             </p>
